4e1dabda92bf26071e9a35e64dd74a555820f129
Nantas Nardelli <nantas.nardelli@gmail.com>
2014-11-01 19:24:45 +0000

    Add documentation

--- gre2anki.py
+++ gre2anki.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
+"""
+Overview
+========
 
+Chooses a GRE word from a list of nearly 4000 words and outputs
+its description. You can then copy that in Anki or other SRS software.
+Every time you run the script, the random word will be added to
+`archive.txt`, which will stop the word from appearing again. In case
+you want to start over, just delete `archive.txt`.
+
+Usage
+-----
+
+`python gre2anki.py`
+
+"""
 import argparse
 import random
 
@@ -25,6 +40,9 @@ class GRE2Anki(object):
         self.load_archived()
 
     def load(self, path):
+        """
+        Load file and split the lines.
+        """
         try:
             f = open(path)
         except IOError:
@@ -32,6 +50,9 @@ class GRE2Anki(object):
         self.lines = f.readlines()
 
     def load_archived(self, path_archive="default"):
+        """
+        Load list of archived words into class.
+        """
         if path_archive == "default":
             path_archive = self.path_archive
         print("Loading archive at {}".format(path_archive))
